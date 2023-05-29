9602a3f16c7d7223cd75f1abecc9fed7cbf2b198
RANIT SARKAR <85814394+RANIT001@users.noreply.github.com>
2023-05-29 11:01:42 +0530

    bug fix

--- main.py
+++ main.py
@@ -9,28 +9,12 @@ class WritingApp:
         self.text = tk.Text(self.master, height=20, width=50)
         self.text.pack()
 
-        self.timer_interval2 = 8000
-        self.timer_interval1=5000
         self.timer_interval = 10000  # Time interval in milliseconds (e.g., 1000 = 1 second)
         self.timer = None
-        self.new_color8 = 'red'
-        self.new_color5 = 'green'
 
-        self.warn1()
-        self.warn2()
         self.start_timer()
         self.master.bind("<Key>", self.reset_timer)
 
-    def warn1(self):
-        self.text.after(self.timer_interval2, self.change_text_color2)
-    def warn2(self):
-        self.text.after(self.timer_interval1, self.change_text_color1)
-
-    def change_text_color2(self):
-        self.text.config(fg=self.new_color8)  # Change the text color
-
-    def change_text_color1(self):
-        self.text.config(fg=self.new_color5)  # Change the text color
 
     def start_timer(self):
         self.timer = self.master.after(self.timer_interval, self.delete_text)
@@ -39,8 +23,6 @@ class WritingApp:
         if self.timer:
             self.master.after_cancel(self.timer)
         self.start_timer()
-        self.warn1()
-        self.warn2()
 
 
     def delete_text(self):
